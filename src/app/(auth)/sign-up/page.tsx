7176bf67f42328fe2878cd7f633942a2e752c0fb
Raphael Borba <raphaelbsx@gmail.com>
2024-01-26 11:17:55 -0300

    feat: create signUp page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,137 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { z } from "zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 
+const formZodSchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(6)
+})
 
-export default function SignUpPage(){
+type SignInType = z.infer<typeof formZodSchema>
 
+export default function SignUpPage() {
 
-    return(
-        <main>
-            
+    const form = useForm<SignInType>({
+        resolver: zodResolver(formZodSchema)
+    })
+
+    const onSubmit = async (data: SignInType) => {
+        console.log(data);
+
+    }
+
+    return (
+
+        <main className="h-screen flex justify-center items-center gap-10">
+            <section className="text-center flex flex-col max-w-lg items-center">
+                <Image
+                    src="/authHero.png"
+                    alt="Hero Auth"
+                    width={500}
+                    height={500}
+                />
+                <h1 className="text-muted-foreground text-3xl"><b className="text-primary">WealthLearn</b>: A new way to learn to invest!</h1>
+                <span className="text-muted-foreground mt-4">Here you can see how investments work over time.</span>
+            </section>
+            <article className="max-w-lg w-full">
+
+                <h1 className="text-center">LOGO</h1>
+                <Form {...form}>
+                    <form className="flex flex-col gap-6" onSubmit={form.handleSubmit(onSubmit)}>
+
+                        {/* USERNAME */}
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="text"
+                                            className="outline-none"
+                                            placeholder="Username"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            className="outline-none"
+                                            placeholder="Email"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            className="outline-none"
+                                            placeholder="Password"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
+            </article>
         </main>
+
     )
 }
+
+/* 
+
+                    <div>
+                        <Label htmlFor="name" >Name</Label>
+                        <Input className="outline-none" id="name" type="text" {...register("name")} />
+                    </div>
+                    <div>
+                        <Label htmlFor="name" >Email</Label>
+                        <Input className="outline-none" id="email" type="email" {...register("email")} />
+                    </div>
+                    <div>
+                        <Label htmlFor="name" >Password</Label>
+                        <Input className="outline-none" id="password" type="password" {...register("password")} />
+                    </div>
+                    <Button type="submit">Submit</Button>
+                </form>
+                 */
