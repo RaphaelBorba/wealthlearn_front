1882e8379dd182da091295ccccee77a6cbc2e240
Raphael Borba <raphaelbsx@gmail.com>
2024-02-18 10:47:32 -0300

    feat: start create compostTax page

---
+++ src/app/(tools)/calculadora/juros-compostos/page.tsx
@@ -0,0 +1,86 @@
+'use client'
+import CompostTaxCalculator from "@/components/shared/Calculators/CompostTax/CompostTaxForm"
+import { ShowResponse } from "@/components/shared/Calculators/SimpleTax/ShowResponse"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { CalculatorResponse } from "@/types/calculators"
+import { useState } from "react"
+
+export default function JurosCompostosPage() {
+
+  const [calculatorResponse, setCalculatorResponse] = useState<null | CalculatorResponse>(null)
+
+  return (
+    <main className="wrapper pb-20">
+
+      <div className="calculator">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-primary">Calculadora de Juros Compostos</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CompostTaxCalculator setCalculatorResponse={setCalculatorResponse} />
+          </CardContent>
+          {
+            calculatorResponse === null ?
+              <CardFooter className="flex flex-col items-start gap-4">
+                <h1 className="text-xl font-bold">Como usar a calculadora de juros compostos:</h1>
+                <ul className="ml-5 min-[600px]:ml-10">
+                  <li className="flex gap-2 flex-row">
+                    <p><b className="text-primary font-bold">1 -{" "}</b>{" "}Preencha o campo Valor Inicial</p>
+                  </li>
+                  <li className="flex gap-2 flex-row">
+                    <p><b className="text-primary font-bold">2 -{" "}</b>{" "}Preencha o campo Valor Mensal</p>
+                  </li>
+                  <li className="flex gap-2 flex-row">
+                    <p><b className="text-primary font-bold">3 -{" "}</b>{" "}Preencha a Taxa de juros anual ou mensal</p>
+                  </li>
+                  <li className="flex gap-2 flex-row">
+                    <p><b className="text-primary font-bold">4 -{" "}</b>{" "}Preencha o campo Período em meses ou anos;</p>
+                  </li>
+                  <li className="flex gap-2 flex-row">
+                    <p><b className="text-primary font-bold">5 -{" "}</b>{" "}Clique em Calcular e veja o resultado.</p>
+                  </li>
+                </ul>
+                <h1 className="text-xl font-bold">O que é Juros Compostos</h1>
+                <p>Uma calculadora de juros compostos é uma ferramenta que permite calcular o valor acumulado de um investimento ao longo do tempo, levando em consideração o efeito dos juros sobre juros.</p>
+                <p>No juro composto, o valor do juro é calculado não apenas sobre o valor principal, mas também sobre os juros acumulados em períodos anteriores. A fórmula para calcular o juro composto é:</p>
+                <strong className="text-primary ml-5 min-[600px]:ml-10">A = P * (1 + r)^n</strong>
+                <ul className="ml-5 min-[600px]:ml-10">
+                  <li className="flex">
+                    <strong className="mr-2 w-3 text-primary">A</strong>
+                    <p className=""> = valor acumulado (incluindo o principal e os juros acumulados)</p>
+                  </li>
+                  <li className="flex">
+                    <strong className="mr-2 w-3 text-primary">P</strong>
+                    <p className=""> = valor principal (o valor inicial do investimento)</p>
+                  </li>
+                  <li className="flex">
+                    <strong className="mr-2 w-3 text-primary">r</strong>
+                    <p className=""> = taxa de juros (expressa em decimal, por exemplo, uma taxa de 5% seria 0.05)</p>
+                  </li>
+                  <li className="flex">
+                    <strong className="mr-2 w-3 text-primary">n</strong>
+                    <p className=""> = número de períodos de tempo (como meses ou anos)</p>
+                  </li>
+                </ul>
+                <p>Portanto, uma calculadora de juros compostos permitiria ao usuário inserir o valor principal, a taxa de juros e o número de períodos de tempo, e então calcularia e exibiria o valor acumulado ao longo do tempo.</p>
+                <p>Essa ferramenta pode ser útil para pessoas que desejam entender melhor o crescimento de um investimento ao longo do tempo e o efeito dos juros sobre o saldo acumulado.</p>
+
+              </CardFooter>
+              :
+              <CardFooter className="max-h-[500px]">
+                <ShowResponse data={calculatorResponse} />
+              </CardFooter>
+          }
+        </Card>
+      </div>
+
+    </main>
+  )
+}
---
+++ src/components/shared/Calculators/CompostTax/CompostTaxForm.tsx
@@ -0,0 +1,189 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Dispatch, SetStateAction, useState } from "react"
+import { postCalculatorSimpleTax } from "@/services/Calculators"
+import { CalculatorResponse } from "@/types/calculators"
+
+
+const formZodSchema = z.object({
+  amount: z.number().min(1, "Número deve ser maior que 1!").max(1000000000, "Número deve ser menor que 1000000000"),
+  monthValue: z.number().min(1, "Número deve ser maior que 1").max(10000000, "Número deve ser menor que 10000000"),
+  tax: z.number().min(0.1, "Número deve ser maior que 0.1!").max(100, "Número deve ser menor que 100"),
+  time: z.number().min(1, "Número deve ser maior que 1").max(2000, "Número deve ser menor que 2000"),
+})
+
+type TFormZodSchema = z.infer<typeof formZodSchema>
+
+interface IProps {
+  setCalculatorResponse: Dispatch<SetStateAction<CalculatorResponse | null>>
+}
+
+export default function CompostTaxCalculator({ setCalculatorResponse }: IProps) {
+
+  const [typeTime, setTypeTime] = useState<'year' | 'month'>("year")
+  const [typeTax, setTypeTax] = useState<'year' | 'month'>("year")
+  const [isDisable, setIsDisable] = useState<boolean>(false)
+  const form = useForm<TFormZodSchema>({
+    resolver: zodResolver(formZodSchema),
+  })
+
+  async function onSubmit(form: TFormZodSchema) {
+    setIsDisable(true)
+    const data = { ...form, typeTax, typeTime }
+    console.log(data)
+    //const response = await postCalculatorSimpleTax(data)
+    //setCalculatorResponse(response)
+    setIsDisable(false)
+  }
+
+  function resetForm() {
+    form.reset({ amount: NaN, tax: NaN, time: NaN, monthValue:NaN })
+    form.clearErrors()
+    setCalculatorResponse(null)
+  }
+  return (
+    <Form {...form}>
+      <form className="flex flex-col md:px-12 max-w-5xl mx-auto" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col justify-between gap-5 w-full">
+          <div className="flex flex-col min-[600px]:flex-row justify-around gap-5">
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem className="w-full min-[600px]:w-96">
+                  <FormLabel>Valor Inicial:</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-1">
+
+                      <span className="text-lg bg-secondary w-12 py-1 flex justify-center items-center rounded-md">R$</span>
+                      <Input
+                        disabled={isDisable}
+                        type="number"
+                        className="outline-none"
+                        placeholder="1000,00"
+                        step="0.01"
+                        {...field}
+                        onChange={event => field.onChange(+event.target.value)}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="monthValue"
+              render={({ field }) => (
+                <FormItem className="w-full min-[600px]:w-96">
+                  <FormLabel>Valor Mensal:</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-1">
+
+                      <span className="text-lg bg-secondary w-12 py-1 flex justify-center items-center rounded-md">R$</span>
+                      <Input
+                        disabled={isDisable}
+                        type="number"
+                        className="outline-none"
+                        placeholder="1000,00"
+                        step="0.01"
+                        {...field}
+                        onChange={event => field.onChange(+event.target.value)}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col min-[600px]:flex-row justify-around gap-5">
+            <FormField
+              control={form.control}
+              name="tax"
+              render={({ field }) => (
+                <FormItem className="w-full min-[600px]:w-96">
+                  <FormLabel>Taxa de Juros:</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-1">
+                      <span className="text-lg bg-secondary min-w-8 py-1 flex justify-center items-center rounded-md">%</span>
+                      <Input
+                        disabled={isDisable}
+                        type="number"
+                        className="outline-none"
+                        placeholder="12"
+                        {...field}
+                        onChange={event => field.onChange(+event.target.value)}
+                      />
+                      <Select onValueChange={(e: 'year' | 'month') => setTypeTax(e)} defaultValue={typeTax}>
+                        <SelectTrigger className="w-[120px] focus-visible:ring-transparent">
+                          <SelectValue placeholder="Time" />
+                        </SelectTrigger>
+                        <SelectContent className="mr-8">
+                          <SelectItem value="year">Anual</SelectItem>
+                          <SelectItem value="month">Mensal</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="time"
+              render={({ field }) => (
+                <FormItem className="w-full min-[600px]:w-96">
+                  <FormLabel>Período:</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-row">
+                      <Input
+                        disabled={isDisable}
+                        type="number"
+                        className="outline-none"
+                        placeholder="10"
+                        {...field}
+                        onChange={event => field.onChange(+event.target.value)}
+                      />
+                      <Select onValueChange={(e: 'year' | 'month') => setTypeTime(e)} defaultValue={typeTime}>
+                        <SelectTrigger className="w-[120px] focus-visible:ring-transparent">
+                          <SelectValue placeholder="Time" />
+                        </SelectTrigger>
+                        <SelectContent className="mr-8">
+                          <SelectItem value="year">Ano</SelectItem>
+                          <SelectItem value="month">Mês</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <div className="w-full flex flex-col min-[600px]:flex-row justify-end gap-5 min-[600px]:gap-10 mt-[90px]">
+          <Button disabled={isDisable} className="w-full min-[600px]:w-36" type="submit">CALCULAR</Button>
+          <Button
+            type="reset"
+            className="w-full min-[600px]:w-36"
+            onClick={resetForm}
+            variant="secondary">LIMPAR</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
